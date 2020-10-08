c4587671f6e52509e930c61c821d4fb090b7b404
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-10-08 12:55:50 -0300

    Created generateSalt Function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 import crypto from 'crypto'
+
+function generateSalt(rounds = 12) {
+  if(rounds > 15) {
+    throw new Error(`${rounds} is greater than 15, must be less that 15`)
+  }
+
+  if(typeof rounds !== 'number') {
+    throw new Error('Rounds must be a number')
+  }
+
+  return crypto.randomBytes(Math.ceil(rounds / 2)).toString('hex').slice(0, rounds)
+}
+
+console.log(generateSalt(), generateSalt(15))
